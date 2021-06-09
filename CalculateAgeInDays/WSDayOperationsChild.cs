252207afa8f025806780223bdd2e8004e383d8fc
Gabriela Bardel <gabriela_bardel@edsa.com.ar>
2021-06-09 13:07:08 -0300

    commit

---
+++ CalculateAgeInDays/WSDayOperationsChild.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WSDaysOperations;
+
+namespace AgeToDays
+{
+    public class WSDayOperationsChild : DaysOperations
+    {
+    }
+}
